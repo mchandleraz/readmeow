15e9fd5472e0169e89e4d4dacaf11b581aa5310a
Matt Chandler <mcaz689@gmail.com>
2017-02-06 14:14:25 -0700

    fix api and refactor

--- src/index.js
+++ src/index.js
@@ -7,44 +7,70 @@ const LineByLineReader = require('line-by-line');
 const workingDirectory = process.cwd();
 const filepath = `${workingDirectory}/README.md`;
 const package = require(`${workingDirectory}/package.json`);
+const altTag = 'powered by readmeow';
 
-module.exports = function() {
+let hasCat = false;
+
+readmeow();
+
+function readmeow() {
   requestPromise('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat')
     .then(function(response) {
-      const data = JSON.parse(response).data;
-      const gifMarkdown = `![powered by readmeow](${data.image_url})`;
+      const gifMarkdown = generateReadmeowMarkdown(response);
 
       let readme = new LineByLineReader(filepath);
       let finalMarkdown = '';
-      let hasCat = false;
 
-      readme.on('error', function(err) {
-        if (err.code !== 'ENOENT') {
-          throw err;
-        }
-        readme = fs.createWriteStream(filepath);
-        readme.write(`# ${package.name}\n${gifMarkdown}`);
-        hasCat = true;
+      readme.on('error', function(error) {
+        onLineByLineError(error, gifMarkdown);
       });
-      readme.on('line', function(line) {
-        if (hasCat || (line.indexOf('powered by readmeow') > -1)) {
-          return;
-        }
-
-        if (line.match(/^\#{1,6}/g)) {
-          // TODO: review regex. This might be better: /(^\#{1,6}.*\w$)/g
-          line += '\n' + gifMarkdown + '\n';
-        }
-
-        finalMarkdown += line;
-        hasCat = true;
+      readme.on('line', function (line) {
+        onLineByLineRead(line, gifMarkdown, finalMarkdown);
       });
-      readme.on('end', function() {
-        let _readme = fs.createWriteStream(filepath);
-        _readme.write(finalMarkdown);
+      readme.on('end', function () {
+        if (finalMarkdown.length > 0) {
+          writeReadme(filepath, finalMarkdown);
+        }
       });
     })
-    .catch(function(err) {
-      throw err;
+    .catch(function(error) {
+      throw error;
     });
+}
+
+function generateReadmeowMarkdown(response) {
+  return `![${altTag}](${JSON.parse(response).data.image_url})`;
+}
+
+function onLineByLineError(error, gifMarkdown) {
+  if (error.code !== 'ENOENT') {
+    // if erroror is anything other than "no file":
+    throw error;
+  }
+  // error.code is essentially "no file", so:
+  readme = fs.createWriteStream(filepath);
+  readme.write(`# ${package.name}\n${gifMarkdown}`);
+  hasCat = true;
+}
+
+function writeReadme(path, content) {
+  let _readme = fs.createWriteStream(path);
+  _readme.write(content);
+}
+
+function onLineByLineRead(line, gifMarkdown, finalMarkdown, iterator) {
+  if (line.indexOf(altTag) > -1) {
+    hasCat = true;
+  }
+
+  if (hasCat) {
+    return;
+  }
+
+  if (line.match(/^\#{1,6}/g)) {
+    line += '\n' + gifMarkdown + '\n';
+  }
+
+  finalMarkdown += line;
+  hasCat = true;
 }
